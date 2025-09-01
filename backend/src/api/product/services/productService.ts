cfe4e954f95de5ef7e6979f6d016d5c1c2fa2e40
Hampus Molin <hampus.e.molin@gmail.com>
2025-09-01 21:46:14 +0200

    Refactor service to format responses according to schemas, stronger typing

--- backend/src/api/product/services/productService.ts
+++ backend/src/api/product/services/productService.ts
@@ -1,17 +1,49 @@
-import {Product} from '../../../repo/appdb/_sequelize/models/Product';
-import {ProductColour} from '../../../repo/appdb/_sequelize/models/ProductColour';
-import {Product as ProductInput} from '../types/Product';
+import {
+  Product,
+  Colour,
+  ProductType,
+} from '../../../repo/appdb/_sequelize/models';
+import {Product as ProductInput} from '../types/validation/Product';
 
 export const productService = {
   async createProduct(input: ProductInput) {
-    const product = await Product.create(
-      {
-        name: input.name,
-        productTypeId: input.productTypeId,
-        colours: input.colourIds.map((id) => ({id})),
-      },
-      {include: [ProductColour]},
-    );
+    const product: Product = await Product.create({
+      name: input.name,
+      productTypeId: input.productTypeId,
+    });
+    await product.$set('colours', input.colourIds);
     return product;
+  },
+
+  async listProducts(productTypeId?: number) {
+    const where = productTypeId ? {productTypeId: productTypeId} : {};
+    return Product.findAll({
+      where,
+      attributes: ['id', 'name'],
+      include: [
+        {model: ProductType, attributes: ['productType']},
+        {model: Colour, attributes: ['name'], through: {attributes: []}},
+      ],
+      order: [['createdAt', 'DESC']],
+    });
+  },
+
+  async getProductById(id: number) {
+    return Product.findByPk(id, {
+      include: [
+        {
+          model: ProductType,
+          attributes: {
+            exclude: ['createdAt', 'updatedAt'],
+          },
+        },
+        {
+          model: Colour,
+          attributes: {exclude: ['createdAt', 'updatedAt']},
+          through: {attributes: []},
+        },
+      ],
+      attributes: {exclude: ['createdAt', 'updatedAt']},
+    });
   },
 };
