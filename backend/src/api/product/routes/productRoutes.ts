e904c7200d698cb82e22e41219e6cda41ad80e2c
Hampus Molin <hampus.e.molin@gmail.com>
2025-09-01 21:45:37 +0200

    reconfigure routes to use the wrapper

--- backend/src/api/product/routes/productRoutes.ts
+++ backend/src/api/product/routes/productRoutes.ts
@@ -1,8 +1,15 @@
 import {Router, IRouter} from 'express';
-import {createProduct} from '../controllers/productController';
+import {
+  createProduct,
+  getProductById,
+  listProducts,
+} from '../controllers/productController';
+import {controllerWrapper} from '../../utils/controllerWrapper';
 
 const productRoutes: IRouter = Router();
 
-productRoutes.post('/products', createProduct);
+productRoutes.post('/products', controllerWrapper(createProduct));
+productRoutes.get('/products', controllerWrapper(listProducts));
+productRoutes.get('/products/:id', controllerWrapper(getProductById));
 
 export {productRoutes};
